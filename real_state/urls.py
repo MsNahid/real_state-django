018b4c164f7b9fc88f85572ecfc3c9732f5aeb44
MsNahid <mssnahid729@gmail.com>
2021-01-01 17:16:54 +0600

    added data to template and customizes admin

--- real_state/urls.py
+++ real_state/urls.py
@@ -1,4 +1,4 @@
-"""btre URL Configuration
+"""real_state URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
